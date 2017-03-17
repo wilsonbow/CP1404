d65db392e5573410dcd3f8d63f8b3d44bc3fa276
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-03-17 12:41:36 +1000

    Refactored brokenScore.py to use functions Refactored temperatures.py to use functions

--- Prac03/brokenScore.py
+++ Prac03/brokenScore.py
@@ -6,24 +6,22 @@ Broken program to determine score status
 
 """
 
+def get_grade(score):
+    # Check if input is valid
+    if score < 0 or score > 100:
+        print("Invalid score")
+
+    # If input is valid then test score
+    else:
+        if score > 90:
+            print("Excellent")
+
+        elif score > 50:
+            print("Passable")
+
+        else:
+            print("Bad")
+
 
 score = float(input("Enter score: "))
-
-# Check if input is valid
-if score < 0 or score > 100:
-
-    print("Invalid score")
-
-# If input is valid then test score
-else:
-    if score > 90:
-
-        print("Excellent")
-
-    elif score > 50:
-
-        print("Passable")
-
-    else:
-
-        print("Bad")
+get_grade(score)
--- Prac03/temperatures.py
+++ Prac03/temperatures.py
@@ -1,42 +1,72 @@
 """
-
-CP1404/CP5632 - Practical
-
-Pseudocode for temperature conversion
+Celcius to Fahrenheit conversion
 
 """
 
-__author__ = 'Lindsay Ward'
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
 
 
+def fahrenheit_to_celsius(fahrenheit):
+    # input fahrenheit, return celsius conversion
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
 
-MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
 
-print(MENU)
+def get_celsius():
+    finished = False # finish flag for valid input
+    while not finished:
+        try:
+            celsius = float(input("Celsius: "))
+            finished = True
+        except ValueError:
+            print("Please enter a valid number.")
+    return celsius
 
-choice = input(">>> ").upper()
 
-while choice != "Q":
-    if choice == "C":
+def get_fahrenheit():
+    finished = False
+    while not finished:
+        try:
+            fahrenheit = float(input("Fahrenheit: "))
+            finished = True
+        except ValueError:
+            print("Please enter a valid number.")
+    return fahrenheit
 
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
 
-        print("Result: {:.2f} F".format(fahrenheit))
-
-    elif choice == "F":
-
-        farenheit = float(input("Farenheit: "))
-        celsius = 5 / 9 * (farenheit - 32)
-
-        print("Result: {:.2f} C".format(celsius))
-
-    else:
-
-        print("Invalid option")
-
+def main():
+    # Display menu and get input
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
     print(MENU)
-
     choice = input(">>> ").upper()
 
-print("Thank you.")
+    # Infinite while loop. Terminates when "Q" entered
+    while choice != "Q":
+        if choice == "C":
+
+            celsius = get_celsius()
+            fahrenheit = celsius_to_fahrenheit(celsius)
+
+            print("Result: {:.2f} F".format(fahrenheit))
+
+        elif choice == "F":
+
+            fahrenheit = get_fahrenheit()
+            celsius = fahrenheit_to_celsius(fahrenheit)
+
+            print("Result: {:.2f} C".format(celsius))
+
+        else:
+
+            print("Invalid option")
+
+        print(MENU)
+        choice = input(">>> ").upper()
+
+    # Print when loop terminates (Quit)
+    print("Thank you.")
+
+main()
