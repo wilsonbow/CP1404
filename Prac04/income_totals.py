bdecab14007dcdb8015fa9ee5ec1627836915cb1
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-03-24 13:24:51 +1000

    Completed income_totals.py

---
+++ Prac04/income_totals.py
@@ -0,0 +1,41 @@
+"""
+
+CP1404/CP5632 Practical
+
+Starter code for cumulative total income program
+
+"""
+
+
+
+
+
+def main():
+
+    incomes = []
+
+    num_months = int(input("How many months? "))
+
+
+
+    for month in range(1, num_months + 1):
+
+        income = float(input("Enter income for month {}: ".format(month)))
+
+        incomes.append(income)
+
+    print_report(incomes, num_months)
+
+
+def print_report(incomes, num_months):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, num_months + 1):
+        income = incomes[month - 1]
+
+        total += income
+
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
