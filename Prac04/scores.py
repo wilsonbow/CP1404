33ff47d51f58e7b51883d83e94c1ccd021c7dca5
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-04-03 13:22:01 +1000

    Fixed scores.py

--- Prac04/scores.py
+++ Prac04/scores.py
@@ -7,6 +7,8 @@ converts the rest of the lines from a list of strings into a list of numbers,
 which it then prints with the maximum value for that subject.
 Nice. Except, it’s broken! It reads the lists per user not per subject so the results are incorrect.
 Use the debugger to follow what it's doing... then fix it.
+
+Fixed!
 """
 
 
@@ -16,16 +18,25 @@ def main():
     print(scores_data)
     subjects = scores_data[0].strip().split(",")
     score_values = []
+
+    # Store scores in a list of lists
     for score_line in scores_data[1:]:
         score_strings = score_line.strip().split(",")
         score_numbers = [int(value) for value in score_strings]
         score_values.append(score_numbers)
     scores_file.close()
+
+    # Print scores per subject
     for i in range(len(subjects)):
         print(subjects[i], "Scores:")
-        for score in score_values[i]:
+        subject_scores = []
+        for score_number in range(len(score_values)):
+            score = score_values[score_number][i]
+            subject_scores.append(score)
             print(score)
-        print("Max:", max(score_values[i]))
+        print("Max:", max(subject_scores))
+        print("Min:", min(subject_scores))
+        print("Average:",sum(subject_scores)/len(subject_scores))
         print()
 
 
