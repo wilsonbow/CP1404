ba956c44776a18fc9bf5ae17dd4280bdeb0153f2
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-04-16 19:25:16 +1000

    Finished walkthrough task

--- Prac07/used_cars.py
+++ Prac07/used_cars.py
@@ -8,12 +8,21 @@ def main():
     """Demo test code to show how to use car class."""
     bus = Car(180)
     bus.drive(30)
+    bus.name = "Bus 1"
     print("fuel =", bus.fuel)
     print("odo =", bus.odometer)
     print(bus)
 
     print("Car {}, {}".format(bus.fuel, bus.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=bus))
+
+    limo = Car(100)
+    limo.add_fuel(20)
+    limo.name = "Limo 1"
+    print("Limo fuel: {}".format(limo.fuel))
+    limo.drive(115)
+    print("Limo odo: {}".format(limo.odometer))
+
 
 
 main()
