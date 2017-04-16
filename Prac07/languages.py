a69b9991ea088e15765cf40227b57daf5140b17d
Wilson Bow <wilson.bow@my.jcu.edu.au>
2017-04-16 19:54:38 +1000

    Added functionality to print out all dynamically typed languages

--- Prac07/languages.py
+++ Prac07/languages.py
@@ -6,3 +6,10 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
 print(ruby)
+
+my_languages = [ruby, python, vb]
+print("The dynamically typed languages are:")
+for language in my_languages:
+    if language.typing == "Dynamic":
+        print(language.name)
+
